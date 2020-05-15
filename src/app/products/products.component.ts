76d554be35e850e3edc868236d35647b8632f7b9
Mithilesh <mithilesh@backbase.com>
2020-05-15 22:28:12 +0200

    Added SwitchMap to subscribe two obeservables

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -3,6 +3,7 @@ import { ProductService } from '../common/service/product.service';
 import { CategoryService } from '../common/service/category.service';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../models/product';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-products',
@@ -11,18 +12,34 @@ import { Product } from '../models/product';
 })
 export class ProductsComponent implements OnInit {
 
-  products: Product[] =[];
-  filteredProducts: Product[]=[];
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
   categories$;
   filteringCategory;
-  constructor(private productsService: ProductService, private categoryService: CategoryService, private activatedRoute: ActivatedRoute) { 
-    productsService.getAllProducts().subscribe(p  => this.products = p);
-    this.categories$ = categoryService.getCategories().snapshotChanges();
-    this.activatedRoute.queryParamMap
-    .subscribe(params  => {
+  constructor(private productsService: ProductService, private categoryService: CategoryService, private activatedRoute: ActivatedRoute) {
+    
+    productsService.getAllProducts().pipe(
+      switchMap(products => {
+        this.products = products;
+        return this.activatedRoute.queryParamMap;
+      })
+    ).subscribe(params => {
       this.filteringCategory = params.get('category');
-       this.filteredProducts = (this.filteringCategory)  ? this.products.filter(p => p.category === this.filteringCategory) : this.products;
+      this.filteredProducts = (this.filteringCategory) ? this.products.filter(p => p.category === this.filteringCategory) : this.products;
     });
+    
+    //Old Way to subscribe
+    // productsService.getAllProducts().subscribe(p => {
+    //   this.products = p;
+
+    //   this.activatedRoute.queryParamMap
+    //     .subscribe(params => {
+    //       this.filteringCategory = params.get('category');
+    //       this.filteredProducts = (this.filteringCategory) ? this.products.filter(p => p.category === this.filteringCategory) : this.products;
+    //     });
+    // });
+    this.categories$ = categoryService.getCategories().snapshotChanges();
+
   }
 
   ngOnInit(): void {
