7a12bf49be01f0a6edf74e24caad5f3c3a396e42
Mithilesh <mithilesh@backbase.com>
2020-05-19 02:04:29 +0200

    Changes method name properly

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -51,7 +51,7 @@ export class ProductsComponent implements OnInit,  OnDestroy {
   }
 
   async ngOnInit() {
-     this.subscription = (await this.shoppingCartService.getCartOld()).subscribe((cart) => {
+     this.subscription = (await this.shoppingCartService.getCart()).subscribe((cart) => {
        this.cart = cart;
       });  
   }
